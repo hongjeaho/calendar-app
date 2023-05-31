b2c47b64ba7be48a7bf2553ff9ce6c35e43c6119
홍재호 <kansin88@drmamma.co.kr>
2023-05-31 22:04:16 +0900

    fix demo data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { type AccommoDatePrice } from './type'
-import { format, getDate, parse } from 'date-fns'
+import { format, getDate, parse, differenceInCalendarDays, addDays } from 'date-fns'
 import Calendar from './components/Calendar'
 
-function getDemoData(start:string, end:string, dateFormat:string) {
-  
+function getDemoData(start: string, end: string, dateFormat: string) {
   const toDay = new Date()
-  
-  const sampleData: AccommoDatePrice[] = []
-  const firstDate = parse(start, dateFormat, toDay) 
-  const lastDate = parse(end, dateFormat, toDay)
 
-  for (let day = getDate(firstDate); day <= lastDate.getDate(); day++) {
-    const stock = day - 5 < 0 ? 0 : day - 5 
+  const sampleData: AccommoDatePrice[] = []
+  const firstDate = parse(start, dateFormat, toDay)
+  const lastDate = parse(end, dateFormat, toDay)
+  const diffDays = differenceInCalendarDays(lastDate, firstDate)
+
+  for (let day = getDate(firstDate); day <= diffDays; day++) {
+    const stock = day - 5 < 0 ? 0 : day - 5
     sampleData.push({
-      date: format(new Date(toDay.getFullYear(), toDay.getMonth(), day), 'yyyy.MM.dd'),
+      date: format(addDays(firstDate, day), dateFormat),
       stock,
       price: parseInt(`${day}000`),
     })
@@ -25,12 +25,17 @@ function getDemoData(start:string, end:string, dateFormat:string) {
 
 function App() {
   const startDate = '2023.05.05'
-  const endDate = '2023.05.22'
+  const endDate = '2023.09.22'
   const dateFormat = 'yyyy.MM.dd'
 
   return (
     <div>
-      <Calendar accommoDatePrice={getDemoData(startDate, endDate, dateFormat)} dateFormat={dateFormat} startDate={startDate}  endDate={endDate}/>
+      <Calendar
+        accommoDatePrice={getDemoData(startDate, endDate, dateFormat)}
+        dateFormat={dateFormat}
+        startDate={startDate}
+        endDate={endDate}
+      />
     </div>
   )
 }
